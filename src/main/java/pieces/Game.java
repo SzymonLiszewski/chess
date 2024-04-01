7faf4c5d193924fa04dc31de1d7b96627d7eb4a5
Szymon Liszewski <szymonlis21@gmail.com>
2024-04-01 17:36:52 +0200

    add pieces, fix pawn moves

--- src/main/java/pieces/Game.java
+++ src/main/java/pieces/Game.java
@@ -9,6 +9,7 @@ import javafx.scene.image.Image;
 import pieces.*;
 public class Game {
     public Map<Position, Piece> pieces;
+    public char onMove;
 
     public Game() {
         pieces = new HashMap<>();
@@ -20,13 +21,30 @@ public class Game {
         pieces.put(new Position(7,1),new Knight(new Position(7,1),new Image(getClass().getResource("/images/bN.png").toExternalForm()), this));
         pieces.put(new Position(4,1),new Queen(new Position(4,1),new Image(getClass().getResource("/images/bQ.png").toExternalForm()), this));
         pieces.put(new Position(5,1),new King(new Position(5,1),new Image(getClass().getResource("/images/bK.png").toExternalForm()), this));
-        pieces.put(new Position(1,2),new Pawn(new Position(1,2),new Image(getClass().getResource("/images/bP.png").toExternalForm()), this));
-        pieces.put(new Position(2,2),new Pawn(new Position(2,2),new Image(getClass().getResource("/images/bP.png").toExternalForm()), this));
-        pieces.put(new Position(3,2),new Pawn(new Position(3,2),new Image(getClass().getResource("/images/bP.png").toExternalForm()), this));
-        pieces.put(new Position(4,2),new Pawn(new Position(4,2),new Image(getClass().getResource("/images/bP.png").toExternalForm()), this));
-        pieces.put(new Position(5,2),new Pawn(new Position(5,2),new Image(getClass().getResource("/images/bP.png").toExternalForm()), this));
-        pieces.put(new Position(6,2),new Pawn(new Position(6,2),new Image(getClass().getResource("/images/bP.png").toExternalForm()), this));
-        pieces.put(new Position(7,2),new Pawn(new Position(7,2),new Image(getClass().getResource("/images/bP.png").toExternalForm()), this));
-        pieces.put(new Position(8,2),new Pawn(new Position(8,2),new Image(getClass().getResource("/images/bP.png").toExternalForm()), this));
+        pieces.put(new Position(1,2),new Pawn(new Position(1,2),new Image(getClass().getResource("/images/bP.png").toExternalForm()), this, 'b'));
+        pieces.put(new Position(2,2),new Pawn(new Position(2,2),new Image(getClass().getResource("/images/bP.png").toExternalForm()), this, 'b'));
+        pieces.put(new Position(3,2),new Pawn(new Position(3,2),new Image(getClass().getResource("/images/bP.png").toExternalForm()), this, 'b'));
+        pieces.put(new Position(4,2),new Pawn(new Position(4,2),new Image(getClass().getResource("/images/bP.png").toExternalForm()), this, 'b'));
+        pieces.put(new Position(5,2),new Pawn(new Position(5,2),new Image(getClass().getResource("/images/bP.png").toExternalForm()), this, 'b'));
+        pieces.put(new Position(6,2),new Pawn(new Position(6,2),new Image(getClass().getResource("/images/bP.png").toExternalForm()), this, 'b'));
+        pieces.put(new Position(7,2),new Pawn(new Position(7,2),new Image(getClass().getResource("/images/bP.png").toExternalForm()), this, 'b'));
+        pieces.put(new Position(8,2),new Pawn(new Position(8,2),new Image(getClass().getResource("/images/bP.png").toExternalForm()), this, 'b'));
+        pieces.put(new Position(3,8),new Bishop(new Position(3,8),new Image(getClass().getResource("/images/wB.png").toExternalForm()), this));
+        pieces.put(new Position(6,8),new Bishop(new Position(6,8),new Image(getClass().getResource("/images/wB.png").toExternalForm()), this));
+        pieces.put(new Position(8,8),new Rook(new Position(8,8),new Image(getClass().getResource("/images/wR.png").toExternalForm()), this));
+        pieces.put(new Position(1,8),new Rook(new Position(1,8),new Image(getClass().getResource("/images/wR.png").toExternalForm()), this));
+        pieces.put(new Position(2,8),new Knight(new Position(2,8),new Image(getClass().getResource("/images/wN.png").toExternalForm()), this));
+        pieces.put(new Position(7,8),new Knight(new Position(7,8),new Image(getClass().getResource("/images/wN.png").toExternalForm()), this));
+        pieces.put(new Position(4,8),new Queen(new Position(4,8),new Image(getClass().getResource("/images/wQ.png").toExternalForm()), this));
+        pieces.put(new Position(5,8),new King(new Position(5,8),new Image(getClass().getResource("/images/wK.png").toExternalForm()), this));
+        pieces.put(new Position(1,7),new Pawn(new Position(1,7),new Image(getClass().getResource("/images/wP.png").toExternalForm()), this, 'w'));
+        pieces.put(new Position(2,7),new Pawn(new Position(2,7),new Image(getClass().getResource("/images/wP.png").toExternalForm()), this, 'w'));
+        pieces.put(new Position(3,7),new Pawn(new Position(3,7),new Image(getClass().getResource("/images/wP.png").toExternalForm()), this, 'w'));
+        pieces.put(new Position(4,7),new Pawn(new Position(4,7),new Image(getClass().getResource("/images/wP.png").toExternalForm()), this, 'w'));
+        pieces.put(new Position(5,7),new Pawn(new Position(5,7),new Image(getClass().getResource("/images/wP.png").toExternalForm()), this, 'w'));
+        pieces.put(new Position(6,7),new Pawn(new Position(6,7),new Image(getClass().getResource("/images/wP.png").toExternalForm()), this, 'w'));
+        pieces.put(new Position(7,7),new Pawn(new Position(7,7),new Image(getClass().getResource("/images/wP.png").toExternalForm()), this, 'w'));
+        pieces.put(new Position(8,7),new Pawn(new Position(8,7),new Image(getClass().getResource("/images/wP.png").toExternalForm()), this, 'w'));
+        onMove = 'w';
     }
 }
--- src/main/java/pieces/Pawn.java
+++ src/main/java/pieces/Pawn.java
@@ -6,8 +6,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Pawn extends Piece{
-    public Pawn(Position position, Image image, Game game) {
+    char color;
+    public Pawn(Position position, Image image, Game game, char color) {
         super(position, image, game);
+        this.color = color;
     }
 
     @Override
@@ -15,8 +17,15 @@ public class Pawn extends Piece{
         int x = this.position.X;
         int y = this.position.Y;
         List<Position> moves = new ArrayList<>();
-        if (y+1<=8){
-            moves.add(new Position(x,y+1));
+        if (color == 'b'){
+            if (y+1<=8){
+                moves.add(new Position(x,y+1));
+            }
+        }
+        else if (color == 'w'){
+            if (y-1>=1){
+                moves.add(new Position(x,y-1));
+            }
         }
         return moves;
     }
